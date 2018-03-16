ced6b8bc557386352901774d8786e3add8aa0d19
dustin lab <yanshuoc@gmail.com>
2018-03-16 10:50:29 +0800

    modify postprocess

--- phySCNAClonal/postprocess/write_results.py
+++ phySCNAClonal/postprocess/write_results.py
@@ -5,39 +5,14 @@ from phySCNAClonal.postprocess.pwgsresults.result_munger import ResultMunger
 from phySCNAClonal.postprocess.pwgsresults.json_writer import JsonWriter
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Write JSON files describing trees',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        '--include-stripe-names',
-        dest='includeStripeNames',
-        action='store_true',
-        help='Include stripe names in output (which may be sensitive data)')
-    parser.add_argument('datasetName', help='Name identifying dataset')
-    parser.add_argument('treeFile', help='File containing sampled trees')
-    parser.add_argument('segPoolFile', help='File containing segment pool')
-    parser.add_argument(
-        'treeSummaryOutput',
-        help='Output file for JSON-formatted tree summaries')
-    parser.add_argument(
-        'mutlistOutput',
-        help='Output file for JSON-formatted list of mutations')
-    parser.add_argument(
-        'mutassOutput',
-        help=
-        'Output file for JSON-formatted list of SSMs and CNVs assigned to each subclone'
-    )
-    args = parser.parse_args()
-
+def process(args):
     # 此处应该生成关于目标的copyNumber, phi等信息
     summaries, mutlist, mutass, params = ResultGenerator().generate(
         args.treeFile, args.segPoolFile, args.includeStripeNames)
 
     munger = ResultMunger(summaries, mutlist, mutass)
 
-# 此处应该不必去除
+    # 此处应该不必去除
     # summaries, mutass = munger.remove_small_nodes(args.min_ssms)
     # 去除多克隆和超级克隆，此处可以使用
     munger.remove_superclones()
