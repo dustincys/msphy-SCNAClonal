5d00f30aa58f19e1b6dddda08f7e15ef380567dc
Dustin <yanshuochu@163.com>
2018-09-13 10:22:27 +0800

    update result generator

--- phySCNAClonal/postprocess/pwgsresults/result_generator.py
+++ phySCNAClonal/postprocess/pwgsresults/result_generator.py
@@ -10,14 +10,14 @@ import json
 
 
 class ResultGenerator(object):
-    def generate(self, treeFile, segPoolLFile, includeStripeNames):
+    def generate(self, treeFile, SCNALFile, includeStripeNames):
         """
-        segPoolL: load the segment information
+        Load SCNA data from SCNALFile, write population and data parameters
+        into SCNA data file
         """
         reader = util2.TreeReader(treeFile)
-        firstTree = next(reader.load_trees())
-        # cnv_logical_physical_mapping = json.loads(reader.read_extra_file('cnv_logical_physical_mapping.json'))
         try:
+            # to record params, manually
             params = json.loads(reader.read_extra_file('params.json'))
         except KeyError:
             # File not present in archive, likely because it originates from an
@@ -25,32 +25,70 @@ class ResultGenerator(object):
             params = {}
         reader.close()
 
-        segPool = self.__load_segPoolL_from_pkl(segPoolLFile)
+        SCNAPool, isStripe = self.__load_SCNAL_from_pkl(SCNALFile)
 
-        # 注意此处是否添加add stripe name
-        mutList = self._list_mutations(firstTree, segPool, includeStripeNames)
         summaries = {}
         allMutAss = {}
-        for idx, llh, pops, mutAss, structure in self._summarize_all_pops(
-                treeFile):
+        allMutDataParam = {}
+
+        for idx, llh, dp, pops, mutAss, structure, mutPops in self._summarize_all_pops(
+            treeFile):
             summaries[idx] = {
                 'llh': llh,
                 'structure': structure,
                 'populations': pops,
             }
+            allMutDataParam[idx] = dp
             allMutAss[idx] = mutAss
 
-        return summaries, mutList, allMutAss, params
+            self._update_SCNAPool(mutPops, dp, SCNAPool, isStripe)
 
-    def _summarize_all_pops(self, treeFile):
+        return summaries, mutList, allMutAss, params, SCNAPool, isStripe
+
+    def _update_SCNAPool(self, mutPops, dp, SCNAPool, isStripe):
+
+        if isStripe:
+            for stripe, sdp, idx in zip(
+                SCNAPool.stripes, dp, range(SCNAPool.stripes)):
+                assert stripe.sid == dp[1]
+                assert idx == int(dp[0])
+
+                pop = mutPops[dp[1]]
+
+                for segIdx in stripe.segsIdxL:
+                    SCNAPool.segPool.segments[segIdx].copyNumber = in(dp[2])
+                    SCNAPool.segPool.segments[segIdx].genotype = str(dp[2])
+                    SCNAPool.segPool.segments[segIdx].phi = pop
+                    pass
+                pass
+
+        else:
+            for segment, sdp, idx in zip(
+                SCNAPool.segments, dp, range(SCNAPool.segments)):
+
+                segmentId = "{0}_{1}_{2}".format(
+                    segment.chromName, str(segment.start), str(segment.end))
+                assert segmentId == dp[1]
+                assert idx == int(dp[0])
+
+                pop = mutPops[dp[1]]
+
+                segments.copyNumber = in(dp[2])
+                segments.genotype = str(dp[2])
+                segments.phi = pop
+                pass
+
+    def _summarize_all_pops(self, treeFile, isStripe):
+        # 此处根据SCNAPool的类型进行生成DataParameter
         reader = util2.TreeReader(treeFile)
-        for idx, llh, tree in reader.load_trees_and_metadata(
+        for idx, llh, tree, dp in reader.load_trees_and_metadata(
                 removeEmptyVertices=True):
-            yield (idx, llh) + self._summarize_pops(tree)
+            yield (idx, llh, dp) + self._summarize_pops(tree)
         reader.close()
 
     def _summarize_pops(self, tree):
         pops = {}
+        mutPops = {}
         structure = defaultdict(list)
         # Note that there will be an entry in mutAssignments for a given
         # subclone only if it has at least one SSM or CNV. This assumption
@@ -67,18 +105,19 @@ class ResultGenerator(object):
             cellPrev = vertex.param
             currentIdx = idx[0]
 
-            stripeNum = 0
+            SCNANum = 0
             for mut in mutations:
                 # 此处最好要加入segIdx，直接进行后续的分析
                 # 需要记录位置
+                mutPops[mut.sid] = cellPrev
                 mutAssignments[currentIdx].append(mut.sid)
-                stripeNum += 1
+                SCNANum += 1
 
             # Preorder traversal is consistent with printo_latex.py, meaning index
             # values should correspond to same vertices.
             pops[currentIdx] = {
                 'cellular_prevalence': cellPrev,
-                'stripe_number': stripeNum,
+                'SCNA_number': SCNANum,
             }
 
             # Visit children in order of decreasing phi.
@@ -92,54 +131,13 @@ class ResultGenerator(object):
                 _traverse_r(child, currentIdx)
 
         _traverse_r(tree.root['node'], None)
-        return (pops, mutAssignments, structure)
+        return (pops, mutAssignments, structure, mutPops)
 
-    def _list_mutations(self, tree, segPool, includeStripeNames,
-                        includeSegmentList=True):
-        stripes = {}
-
-        def _getSegInfo(segIdxL, segL):
-            segs = []
-            for idx in segIdxL:
-                info = {
-                    'name':segL[idx].name,
-                    'chrom_name':segL[idx].chrom_name,
-                    'start':segL[idx].start,
-                    'end':segL[idx].end,
-                    'tumor_read_number': segL[idx].tReadNum,
-                    'normal_read_number': segL[idx].nNeadNum,
-                    'gc': segL[idx].gc,
-                }
-                segs.append(info)
-
-            return segs
-
-        def _traverse(node):
-            for mut in node['node'].get_data():
-                stripes[mut.sid] = {
-                    'tumor_read_number': mut.tReadNum,
-                    'normal_read_number': mut.nNeadNum,
-                    'copy_number': mut.copyNumber,
-                    'genotype': mut.genotype,
-                    'tag': mut.tag,
-                }
-                if includeStripeNames:
-                    stripes[mut.sid]['name'] = mut.name
-                if includeSegmentList:
-                    stripes[mut.sid]['segment_index'] = mut.segIdxL.split(",")
-                    stripes[mut.sid]['segment_info'] = _getSegInfo(mut.segIdxL.split(","), segL)
-
-
-            for child in node['children']:
-                _traverse(child)
-
-        _traverse(tree.root)
-
-        return { 'stripes': stripes }
-
-    def __load_segPoolL_from_pkl(self, inputFilePath):
-
+    def __load_SCNAL_from_pkl(self, inputFilePath):
         inputFile = open(inputFilePath, 'rb')
-        segPoolL = pkl.load(inputFile)
-
-        return segPoolL[-1]
+        SCNAPoolL = pkl.load(inputFile)
+        lastSCNAPool = SCNAPoolL[-1]
+        if type(SCNAPool).__name__ == "StripePool":
+            return lastSCNAPool, True
+        elif type(SCNAPool).__name__ == "SegmentPool":
+            return lastSCNAPool, False
