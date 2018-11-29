1542e9808a173b026e04a4f8f6d0e7e6e63979a2
dustin <yanshuochu@qq.com>
2018-11-29 19:11:51 +0800

    change to full time order

--- phySCNAClonal/model/ordertransform.py
+++ phySCNAClonal/model/ordertransform.py
@@ -19,107 +19,7 @@ from os.path import commonprefix
 import numpy as np
 
 
-class C2T(object):
-
-    """transform crossing rule to time ordered rule
-    The configuration file in following format:
-
-    # rule 1
-    1   2   5   6   8
-    5   8   6   1   2
-    8   6   5   2   1
-    # rule 2
-    3   4   7   0
-    3   7   4   0
-    3   7   4   0
-
-    Each digit is the index j of variant.
-    Each line mean the phi_j reverse order of each sample $\theVector{\varkappa}
-    _{k,l}$.
-    """
-
-    def __init__(self, configFilePath):
-        """initialize parameter
-
-        :configFilePath: The phi order configuration
-
-        """
-        self._configFilePath = configFilePath
-        # self.varkappaMatrixArray = {}
-        # self.negativeSD = {}
-
-    def toTimeOrder(self, varkappaMatrixArray):
-        """
-        :returns: TODO
-
-        """
-        for varkappaMatrix in varkappaMatrixArray:
-            for ki in range(len(varkappaMatrix)-1):
-                tArray = [len(
-                    set(varkappaMatrix[-1][kj:]) - set(
-                        varkappaMatrix[ki][np.where(
-                            np.array(varkappaMatrix[ki]) ==\
-                            varkappaMatrix[-1][kj])[0][0]:]))
-                    for kj in range(len(varkappaMatrix[-1][:]))]
-
-                for ti in range(len(varkappaMatrix[-1])):
-                    if varkappaMatrix[-1][ti] not in\
-                            negativeSD.keys():
-                        negativeSD[varkappaMatrix[-1][ti]] = set()
-                    for tj in range(ti+1, len(varkappaMatrix[-1])):
-                        if tArray[tj] < tArray[ti]:
-                            if varkappaMatrix[-1][ti] not in\
-                                    negativeSD.keys():
-                                negativeSD[varkappaMatrix[-1][ti]] =\
-                                    set([varkappaMatrix[-1][tj]])
-                            else:
-                                negativeSD[varkappaMatrix[-1][ti]].add(
-                                    varkappaMatrix[-1][tj])
-        idxOrder = sorted(negativeSD.keys())
-        timeOrder = [(idx, len(negativeSD[idx])) for idx in idxOrder]
-
-        return timeOrder, negativeSD
-
-    def read_config_file(self):
-        """TODO: Docstring for __read_config_file.
-
-        :uu: TODO
-        :returns: TODO
-
-        """
-        with open(self._configFilePath) as configFile:
-            nextNewVarkappa = False
-
-            varkappaMatrix = []
-            varkappaMatrixArray = []
-
-            for line in configFile:
-                line = line.strip()
-                if line == "":
-                    continue
-
-                if line.startswith("#"):
-                    nextNewVarkappa = True
-
-                    if varkappaMatrix != []:
-                        varkappaMatrixArray.append(varkappaMatrix)
-                        varkappaMatrix = []
-
-                else:
-                    if not nextNewVarkappa:
-                        nextNewVarkappa = False
-
-                    arrayLine = [int(item) for item in line.split("\t")]
-                    varkappaMatrix.append(arrayLine)
-                pass
-            pass
-
-            if varkappaMatrix != []:
-                varkappaMatrixArray.append(varkappaMatrix)
-
-        return varkappaMatrixArray
-
-class CS2T(C2T):
+class CS2T:
 
     """
     transform crossing rule to time ordered rule and keep the summing rule
@@ -139,87 +39,68 @@ class CS2T(C2T):
     """
 
     def __init__(self, configFilePath):
-        C2T.__init__(self, configFilePath)
+        self._configFilePath = configFilePath
 
-        self.varkappaMatrixArray, self.currentVarkappa, self.phiDL,\
-            self.currentPhiD = self.read_config_file()
 
-        # print self.varkappaMatrixArray
-        # print self.currentVarkappa
-        # print self.phiDL
-        # print self.currentPhiD
+    def transform(self):
 
-        self.timeOrder, self.negativeSD = self.toTimeOrder(
-            self.varkappaMatrixArray, self.currentVarkappa)
+        varkappaMatrixArray, phiDL = self.__read_config_file()
+        timeOrder, negativeSD = self.__toTimeOrder(varkappaMatrixArray, phiDL)
+        # timeOrder = sorted(timeOrder, key=lambda item: (item[1],
+                                                        # currentPhiD[item[0]]))
 
-        self.epsilonDL = []
-        # print self.phiDL
-        for phiDIndex in range(len(self.phiDL)):
-            epsilonD = {}
-            for key in self.phiDL[phiDIndex].keys():
-                epsilonD[key] = ""
-            self.epsilonDL.append(epsilonD)
+        return timeOrder, negativeSD, phiDL
 
-    def toTimeOrder(self, varkappaMatrixArray, currentVarkappa):
+
+    def __toTimeOrder(self, varkappaMatrixArray, phiDL):
         """ tranform to time order
         :returns: time order
 
         """
         negativeSD = {}
-        totalTimeOrderD = {}
+        totalTimeOrderL = []
 
-        for varkappaMatrix in varkappaMatrixArray:
-            for ki in range(len(varkappaMatrix)):
-                currentTempVarkappa = np.array([currentVarkappa[item] for item
-                                                in range(len(currentVarkappa))
-                                                if currentVarkappa[item] in
-                                                varkappaMatrix[ki]])
-                negativeSetList = [set(currentTempVarkappa[kj:]) -
-                                   set(varkappaMatrix[ki][ np.where(
-                                       np.array(varkappaMatrix[ki]) ==
-                                       currentTempVarkappa[kj])[0][0]:]) for kj
-                                   in range(len(currentTempVarkappa[:]))]
+        for ruleIdx, varkappaMatrix in enumerate(varkappaMatrixArray):
+            currentVarkappa = np.array(varkappaMatrix[-1])
+            currentData = [(idx, phiDL[ruleIdx][idx][-1]) for idx in currentVarkappa]
 
-                for kj in range(len(currentTempVarkappa[:])):
-                    if currentTempVarkappa[kj] not in negativeSD.keys():
-                        negativeSD[currentTempVarkappa[kj]] =\
-                            negativeSetList[kj]
+            for si in range(len(varkappaMatrix)-1):
+                targetVarkappa = np.array(varkappaMatrix[-1])
+                targetData = [(idx, phiDL[ruleIdx][idx][si]) for idx in targetVarkappa]
+
+                for kj in range(len(currentVarkappa[:])):
+                    currentSmallS = set([i for i, n in filter(lambda item: item[1] < phiDL[ruleIdx][currentVarkappa[kj]][-1], currentData)])
+                    targetSmallS = set([i for i, n in filter(lambda item: item[1] < phiDL[ruleIdx][currentVarkappa[kj]][si], targetData)])
+                    negativeS = currentSmallS - targetSmallS
+
+                    if currentVarkappa[kj] not in negativeSD.keys():
+                        negativeSD[currentVarkappa[kj]] = negativeS
                     else:
-                        negativeSD[currentTempVarkappa[kj]] = set.union(
-                            negativeSetList[kj],
-                            negativeSD[currentTempVarkappa[kj]])
+                        negativeSD[currentVarkappa[kj]] = set.union(negativeS, negativeSD[currentVarkappa[kj]])
 
-            tempTimeOrder = self.__idxOrder_to_timeOrder(negativeSD,
-                                                          varkappaMatrix[0],
-                                                          currentVarkappa)
-            for tto in tempTimeOrder:
-                totalTimeOrderD[tto[0]] = tto[1]
+            tempTimeOrder = self.__idxOrder_to_timeOrder(negativeSD, currentData)
+            totalTimeOrderL.append(tempTimeOrder)
 
-        timeOrder = [(idx, totalTimeOrderD[idx]) if idx in
-                     totalTimeOrderD.keys() else (idx, 0) for idx in
-                     currentVarkappa]
+        return totalTimeOrderL, negativeSD
 
-        return timeOrder, negativeSD
-
-
-    def __idxOrder_to_timeOrder(self, negativeSD, targetData,
-                                currentVarkappa):
+    def __idxOrder_to_timeOrder(self, negativeSD, currentData):
         tQueue = deque()
         tStack = deque()
-        currentTempVarkappa = np.array([currentVarkappa[item] for item in
-                                        range(len(currentVarkappa)) if
-                                        currentVarkappa[item] in targetData])
-        nsL = [(idx, deepcopy(negativeSD[idx])) if idx in
-                negativeSD.keys() else (idx, set()) for idx in
-                currentTempVarkappa]
+
+        currentVarkappa = sorted(currentData, key=lambda item: item[1], reverse=True)
+
+        nsL = [(idx, deepcopy(negativeSD[idx]), phi) if idx in
+                negativeSD.keys() else (idx, set(), phi) for idx, phi in
+                currentVarkappa]
+
         stackNegativeSet = set()
 
-        for idx, ns in nsL:
+        for idx, ns, phi in nsL:
             if len(stackNegativeSet) == 0:
                 if len(tStack) > 0:
                     tQueue.append(tStack)
                 tStack = deque()
-            tStack.append([idx, ns])
+            tStack.append([idx, ns, phi])
             stackNegativeSet = set.union(ns, stackNegativeSet)
             if idx in stackNegativeSet:
                 stackNegativeSet.remove(idx)
@@ -231,23 +112,108 @@ class CS2T(C2T):
 
         while len(tQueue) != 0:
             tStack = tQueue.popleft()
-            tStack = deque(sorted(tStack, key=lambda item: len(item[1]),
+            tStack = deque(sorted(tStack, key=lambda item: (len(item[1]), item[2]),
                                   reverse=True))
             lastTimeIdxS = set()
+            lastPhi = -1
             while len(tStack) != 0:
                 if len(tStack[-1][1]) != 0:
                     for item in tStack:
                         item[1] = item[1] - lastTimeIdxS
-                    tStack = deque(sorted(tStack, key=lambda item: len(item[1]),
+                    tStack = deque(sorted(tStack, key=lambda item: (len(item[1]), item[2]),
                                           reverse=True))
                     lastTimeIdxS.clear()
                     currentTime = currentTime + 1
+                elif lastPhi != tStack[-1][2]:
+                    currentTime = currentTime + 1
+                else:
+                    pass
 
-                idx, nsl = tStack.pop()
+                idx, nsl, phi = tStack.pop()
                 timeOrder.append((idx, currentTime))
+                lastPhi = phi
                 lastTimeIdxS.add(idx)
 
         return timeOrder
+
+    def __read_config_file(self):
+        """Read config file: index|phi, phi descreasing order
+        # rule 1
+        1|0.8	2|0.7	5|0.4	6|0.3	8|0.2
+        5|0.7	8|0.5	6|0.1	1|0.05	2|0.02
+        8|0.6	6|0.3	5|0.2	2|0.1	1|0.05
+        # rule 2
+        3|0.8	4|0.5	7|0.3	0|0.2
+        3|0.7	7|0.5	4|0.3	0|0.1
+        """
+        with open(self._configFilePath) as configFile:
+            nextNewVarkappa = False
+            nextCurrent = False
+
+            varkappaMatrix = []
+            varkappaMatrixArray = []
+            phiDL = []
+            phiD = {}
+            currentVarkappa = []
+            currentPhiD = {}
+
+            for line in configFile:
+                line = line.strip().lower()
+                if line == "":
+                    continue
+                if line.startswith("#"):
+                    nextNewVarkappa = True
+                    if varkappaMatrix != []:
+                        varkappaMatrixArray.append(varkappaMatrix)
+                        varkappaMatrix = []
+                    if not phiD == {}:
+                        phiDL.append(phiD)
+                        phiD = {}
+                else:
+                    if not nextNewVarkappa:
+                        nextNewVarkappa = False
+                    arrayLine = [int(item.split("|")[0]) for item in
+                                    line.split("\t")]
+                    varkappaMatrix.append(arrayLine)
+                    for item in line.split("\t"):
+                        spItem = item.split("|")
+                        if len(spItem) > 1:
+                            if int(spItem[0]) not in phiD.keys():
+                                phiD[int(spItem[0])] = [float(spItem[1])]
+                            else:
+                                phiD[int(spItem[0])].append(
+                                    float(spItem[1]))
+
+            if varkappaMatrix != []:
+                varkappaMatrixArray.append(varkappaMatrix)
+            if not phiD == {}:
+                phiDL.append(phiD)
+
+        print "____>>> read_config: varkappaMatrixArray____"
+        print varkappaMatrixArray
+        print "_________end read_config:varkappaMatrixArray______________"
+        print "____>>> read_config: phiDL____"
+        print phiDL
+        print "_________end read_config:phiDL______________"
+
+        return varkappaMatrixArray, phiDL
+
+
+class SRTree(object):
+
+    """Summing rule while sampling tree structure"""
+
+    def __init__(self, phiDL):
+        """initialize the phi dictionary """
+        self._phiDL = phiDL
+
+        self._epsilonDL = []
+        for phiDIndex in range(len(self._phiDL)):
+            epsilonD = {}
+            for key in self._phiDL[phiDIndex].keys():
+                epsilonD[key] = ""
+            self._epsilonDL.append(epsilonD)
+
 
     def update_epsilon(self, dataIndex, epsilon):
         """Update epsilon dictionary
@@ -273,119 +239,29 @@ class CS2T(C2T):
                 return True
 
     def __load_most_recent_ancestor(self, epsilonD, dataIndex, epsilon):
-        ''' load most recent ancestor
         '''
+        load most recent ancestor
+        '''
+        # Obtain maximum prefix
+        #
+        # EpsilonD should in the time order primarily and phi reverse order
+        # secondly
+
         cplL = [(key, len(commonprefix([epsilonD[key], epsilon]))) if key !=
-                dataIndex else (dataIndex, 0) for key in epsilonD ]
+                dataIndex else (dataIndex, 0) for key in epsilonD]
         _, (ancestorIndex, cpl) = max(enumerate(cplL), key=lambda v: v[1][1])
         if cpl > 0:
             return ancestorIndex
         else:
             return -1
 
-    def read_config_file(self):
-        """Read config file: index|phi, phi descreasing order
-        # Current
-        3|0.7   8|0.6   7|0.5   4|0.3   6|0.3   5|0.2   0|0.1   2|0.1   1|0.05
-        # rule 1
-        1|0.8   2|0.7   5|0.4   6|0.3   8|0.2
-        5   8   6   1   2
-        # rule 2
-        3   4   7   0
-        """
-        with open(self._configFilePath) as configFile:
-            nextNewVarkappa = False
-            nextCurrent = False
-
-            varkappaMatrix = []
-            varkappaMatrixArray = []
-            phiDL = []
-            phiD = {}
-            currentVarkappa = []
-            currentPhiD = {}
-
-            for line in configFile:
-                line = line.strip().lower()
-                if line == "":
-                    continue
-                if line.startswith("#"):
-                    if line.find("current") >= 0:
-                        nextCurrent = True
-                    else:
-                        nextNewVarkappa = True
-                        if varkappaMatrix != []:
-                            varkappaMatrixArray.append(varkappaMatrix)
-                            varkappaMatrix = []
-                        if not phiD == {}:
-                            phiDL.append(phiD)
-                            phiD = {}
-                else:
-                    if nextCurrent:
-                        currentVarkappa = [int(item.split("|")[0]) for item in
-                                           line.split("\t")]
-                        # print "currentVarkappa"
-                        # print currentVarkappa
-                        for item in line.split("\t"):
-                            spItem = item.split("|")
-                            if len(spItem) > 1:
-                                if int(spItem[0]) not in phiD.keys():
-                                    currentPhiD[int(spItem[0])] =\
-                                        [float(spItem[1])]
-                                else:
-                                    currentPhiD[int(spItem[0])].append(
-                                        float(spItem[1]))
-                        nextCurrent = False
-                    else:
-                        if not nextNewVarkappa:
-                            nextNewVarkappa = False
-                        arrayLine = [int(item.split("|")[0]) for item in
-                                     line.split("\t")]
-                        varkappaMatrix.append(arrayLine)
-                        for item in line.split("\t"):
-                            spItem = item.split("|")
-                            if len(spItem) > 1:
-                                if int(spItem[0]) not in phiD.keys():
-                                    phiD[int(spItem[0])] = [float(spItem[1])]
-                                else:
-                                    phiD[int(spItem[0])].append(
-                                        float(spItem[1]))
-
-            if varkappaMatrix != []:
-                varkappaMatrixArray.append(varkappaMatrix)
-            if not phiD == {}:
-                phiDL.append(phiD)
-
-            if nextCurrent:
-                currentVarkappa = [int(item.split("|")[0]) for item in
-                                    line.split("\t")]
-                for item in line.split("\t"):
-                    spItem = item.split("|")
-                    if len(spItem) > 1:
-                        if int(spItem[0]) not in phiD.keys():
-                            currentPhiD[int(spItem[0])] =\
-                                [float(spItem[1])]
-                        else:
-                            currentPhiD[int(spItem[0])].append(
-                                float(spItem[1]))
-
-        # print "____>>> read_config: varkappaMatrixArray____"
-        # print varkappaMatrixArray
-        # print "_________end read_config:varkappaMatrixArray______________"
-        # print "____>>> read_config: currentVarkappa____"
-        # print currentVarkappa
-        # print "_________end read_config:currentVarkappa______________"
-        # print "____>>> read_config: phiDL____"
-        # print phiDL
-        # print "_________end read_config:phiDL______________"
-
-        return varkappaMatrixArray, currentVarkappa, phiDL, currentPhiD
-
 def main():
     # c2t = C2T("./crossing.text.example")
     # print c2t.toTimeOrder()
     cs2t = CS2T("./summingandcrossing.example.txt")
-    print cs2t.timeOrder
-    print cs2t.negativeSD
+    timeOrder, negativeSD, phiDL = cs2t.transform()
+
+    print timeOrder, negativeSD, phiDL
 
 if __name__ == "__main__":
     main()
